a7f6e3e8d10820903eb7b8e55635872ba821b047
Nguyen-Dang-Trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2024-06-08 17:02:16 +0700

    Update XOR_LinkedList.cpp

--- 2024/Initial/XOR_LinkedList.cpp
+++ 2024/Initial/XOR_LinkedList.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <stdint.h>  // To use uintptr_t
+using namespace std;
 
+
+struct Node {
+    int data;
+    Node * both; // XOR of next and previous
+};
+
+class XORLinkedList{
+    private:
+        Node* head;
+        Node* tail;
+        // XOR function for Node pointers
+        Node* XOR(Node* a, Node* b);
+    public:
+        //Constructor to initialize an empty list
+        XORLinkedList();
+
+        // Print the elements of the list
+        void print_list(); 
+}
+
+XORLinkedList::XORLinkedList(){
+    head = tail = nullptr;
+}
+
+Node* XORLinkedList::XOR(Node* prev, Node* next){
+    /* We use the uintptr_t data type because Bitwise operators require the two operands to be integers.
+       => uintptr_t to convert the address type to integer*/
+    uintptr_t both = (uintptr_t)prev ^ (uintptr_t)next;
+    return (Node*)both; 
+}
+void XORLinkedList::print_list(){
+    // rule: (A ^ B) ^A = B
+    Node * current = head;
+    Node * previous = nullptr;
+    while(current != nullptr){
+        cout << current->data << endl;
+        Node * next = XOR(current->both,previous);
+        previous = current;
+        current = next;
+    }
+}
+
+
+int main(){
+    return 0;
+}
