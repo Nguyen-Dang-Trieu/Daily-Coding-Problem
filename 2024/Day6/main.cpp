ce88496760a0f7f3cccbf18e23854326a987c179
Nguyen-Dang-Trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2024-07-29 13:22:18 +0700

    Update main.cpp

--- 2024/Day6/main.cpp
+++ 2024/Day6/main.cpp
@@ -17,6 +17,7 @@ class XORLinkedList {
         XORLinkedList();
         
         void add_at_tail(int insert_data);
+        void get(int index);
         void print_list();
         
 };
@@ -44,11 +45,30 @@ void XORLinkedList::add_at_tail(int data){
     tail = new_node;
 }
 
+void XORLinkedList::get(int index){
+    if(index < 0){
+        cout << "Index out of bound" << endl;
+        return;
+    }
+    
+    Node* current = head;
+    Node* previous = nullptr;
+    
+    for(int i = 0; i <= index; i++){
+        Node* next = XOR(current->both, previous);
+        if(i == index){
+            cout << current->data << endl;
+        }
+        previous = current;
+        current = next;
+    }
+}
+
 void XORLinkedList::print_list(){
     Node* current = head;
     Node* previous = nullptr;
     while(current != nullptr){
-        cout << current->data << endl;
+        cout << current->data << " ";
         Node* next = XOR(current->both, previous);
         previous = current;
         current = next;
@@ -58,6 +78,11 @@ int main(){
     XORLinkedList list;
     list.add_at_tail(10);
     list.add_at_tail(20);
+    list.add_at_tail(30);
+    list.add_at_tail(40);
     list.print_list();
+    
+    printf("\n");
+    list.get(1);
     return 0;
 }
