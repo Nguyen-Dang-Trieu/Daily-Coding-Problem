43c36aafa67a9a954c595168e1e73fe45a04291f
Nguyen-Dang-Trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2024-10-30 07:20:22 +0700

    Create Sentence Smash.c

---
+++ codewars/8 kyu/Sentence Smash.c
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/c
+
+#include <stdlib.h>
+
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+char *smash (const char *const words[], size_t count)
+{
+  int a;
+	return calloc(1, 1);
+}
